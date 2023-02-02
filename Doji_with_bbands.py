5befd8336cf69053f6e13f9b3535be69fd767ed9
ANURAG AGARWAL <77426821+anurag203@users.noreply.github.com>
2023-02-02 22:41:59 +0530

    updating ,adding stop loss and correcting current price

--- Doji_with_bbands.py
+++ Doji_with_bbands.py
@@ -2,6 +2,7 @@ from blueshift.library.technicals.indicators import bollinger_band, doji
 
 from blueshift.finance import commission, slippage
 from blueshift.api import(  symbol, order_target_percent, set_commission, set_slippage, schedule_function, date_rules, time_rules, get_datetime)
+import talib as ta
 
 def initialize(context):
     # universe selection
@@ -16,7 +17,7 @@ def initialize(context):
                       'ROC_period_short':30, 
                       'ROC_period_long':120, 
                       'BBands_period':300, 
-                      'trade_freq': 5, 
+                      'trade_freq': 1, 
                       'leverage':2,
                       }
 
@@ -25,6 +26,8 @@ def initialize(context):
     context.target_position = dict((security,100000000) for security in context.securities)
     context.stop_loss = dict((security,0) for security in context.securities)
     context.target_price = dict((security,0) for security in context.securities)
+    context.stop_loss_short = dict((security,0) for security in context.securities)
+    context.target_price_short = dict((security,0) for security in context.securities)
 
     # set trading cost and slippage to zero
     set_commission(commission.PerShare(cost=0.0, min_trade_cost=0.0))
@@ -60,15 +63,19 @@ def generate_target_position(context, data):
         if context.signals[security] == context.params['buy_signal_threshold']:
             context.target_position[security] = weight
         elif context.signals[security] == context.params['sell_signal_threshold']:
-            context.target_position[security] = 0 # -weight
+            context.target_position[security] =  -weight
+        else:
+            context.target_position[security] =  0
+        
 
 
 def generate_signals(context, data):
     try:
         price_data = data.history(context.securities, ['open','high','low','close'], context.params['indicator_lookback'], context.params['indicator_freq'])
+        price_data.drop(price_data.index[-1], inplace=True)
         price_data_current = data.current(context.securities, ['open','high','low','close'])
     except:
-        return
+        return 
 
     for security in context.securities:
         px = price_data.xs(security)
@@ -79,16 +86,33 @@ def signal_function(context,px,curr_price,security):
     ind1 = doji(px)
     if(ind1):
         # print("doji ban gayi !!, on", get_datetime())
-        upper, mid, lower = bollinger_band(px.close.values, context.params['BBands_period'])
+        # upper, mid, lower = bollinger_band(px.close.values, context.params['BBands_period'])
+        uppe, mi, lowe = ta.BBANDS(px.close.values, timeperiod=context.params['BBands_period'],nbdevup=1.5,nbdevdn=1.5)
+        upper=uppe[-1]
+        mid=mi[-1]
+        lower=lowe[-1]
+        # print(upper,mid,lower)
         last_px = px.iloc[-1]
         h = last_px['high']
         l = last_px['low']
+        # print("doji high",h,"low",l)
+        # print("current price:-",curr_price)
         if (h < lower):
             if (curr_price['close']  > h):
                 # print("!!!!!!!!!!!!!!!!!!!!!! NOW WE CAN BUY !!!!!!!!!!!!!!!!!!!!!!")
                 context.stop_loss[security]=l
                 context.target_price[security]=(3*(h-l))+h
                 return 1
-        if(curr_price['close'] <=context.stop_loss[security] or curr_price['close'] >= context.target_price[security]):
-            return -1
-    return 0
+        if(l>upper):
+            if(curr_price['close']<l):
+                # print("!!!!!!!!!!!!!!!!!!!!!! NOW WE CAN SELL !!!!!!!!!!!!!!!!!!!!!!")
+                context.stop_loss_short[security]=h
+                context.target_price_short[security]=l-(3*(h-l))
+                return -1
+        if(context.target_position[security] >0):
+            if(curr_price['close'] <=context.stop_loss[security] or curr_price['close'] >= context.target_price[security]):
+                return 0
+        if(context.target_position[security] <0):
+            if(curr_price['close'] >= context.stop_loss_short[security] or curr_price['close'] <= context.target_price_short[security]):
+                return 0
+    return context.signals[security]
